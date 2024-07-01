7a69434e2c79e300e78e2d5be87f36cf47392304
Aaron Tan <18561712+aaronwtan@users.noreply.github.com>
2024-07-01 18:59:37 -0500

    Add exercises from OO Basics: Classes and Objects 2

---
+++ exercises/classes_and_objects_pt2/q5.rb
@@ -0,0 +1,21 @@
+# Using the following code, create a class named Cat that tracks the number
+# of times a new Cat object is instantiated. The total number
+# of Cat instances should be printed when ::total is invoked.
+class Cat
+  @@num_of_cats = 0
+
+  def self.total
+    puts @@num_of_cats
+  end
+
+  def initialize
+    @@num_of_cats += 1
+  end
+end
+
+kitty1 = Cat.new
+kitty2 = Cat.new
+
+Cat.total
+# Expected output:
+# 2
