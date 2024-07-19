cd7582396f54e540543e106c958254c5b4944b3f
Aaron Tan <18561712+aaronwtan@users.noreply.github.com>
2024-07-19 14:23:50 -0500

    Add expanded functionality to play Rock, Paper, Scissors, Lizard, Spock

--- lesson_2/rpsls/rpsls.rb
+++ lesson_2/rpsls/rpsls.rb
@@ -93,23 +93,31 @@ class RPSLSGame
 
     loop do
       set_settings
+      display_welcome_message(settings.game_mode)
       game = Game.new(settings)
       game.start
       break unless play_again?
     end
 
-    display_goodbye_message
+    display_goodbye_message(settings.game_mode)
   end
 
   private
 
-  def display_welcome_message
-    title = "WELCOME TO #{CLASSIC_MODE.upcase}!"
+  def display_welcome_message(game_mode = :rps)
+    title = case game_mode
+            when :rps then "WELCOME TO #{CLASSIC_MODE.upcase}!"
+            when :rpsls then "WELCOME TO #{EXPANDED_MODE.upcase}!"
+            end
+
     display(title)
   end
 
-  def display_goodbye_message
-    prompt("Thanks for playing #{CLASSIC_MODE}. Good bye!")
+  def display_goodbye_message(game_mode = :rps)
+    case game_mode
+    when :rps then prompt("Thanks for playing #{CLASSIC_MODE}. Good bye!")
+    when :rpsls then prompt("Thanks for playing #{EXPANDED_MODE}. Good bye!")
+    end
   end
 
   def set_settings
@@ -137,7 +145,7 @@ class RPSLSGame
 end
 
 class Game
-  include Utility, Comparable
+  include Utility
   attr_reader :player1, :player2, :win_condition, :game_mode
   attr_accessor :round
 
@@ -230,11 +238,11 @@ class Settings
 
   def set_players
     if default
-      player1 = Human.new(default_game: true)
-      player2 = Computer.new
+      player1 = Human.new(game_mode, default_game: true)
+      player2 = Computer.new(game_mode)
       self.players = [player1, player2]
     else
-      2.times { |num| ask_human_or_computer_player(num) }
+      2.times { |num| players[num] = ask_human_or_computer_player(num) }
     end
   end
 
@@ -243,8 +251,8 @@ class Settings
       prompt("Is player #{num + 1} a human or a computer? (h/c)")
       answer = gets.chomp.downcase
 
-      break players[num] = Human.new if Human::VALUES.include?(answer)
-      break players[num] = Computer.new if Computer::VALUES.include?(answer)
+      return Human.new(game_mode) if Human::VALUES.include?(answer)
+      return Computer.new(game_mode) if Computer::VALUES.include?(answer)
 
       prompt('invalid_human_computer')
     end
@@ -286,10 +294,12 @@ end
 class Player
   include Utility
 
+  attr_reader :game_mode
   attr_accessor :name, :move, :score
 
-  def initialize
+  def initialize(game_mode)
     set_name
+    @game_mode = game_mode
     @score = 0
   end
 
@@ -299,15 +309,13 @@ class Player
 end
 
 class Human < Player
+  attr_reader :default_game
+
   VALUES = ['h', 'human']
 
-  def initialize(default_game: false)
-    if default_game
-      set_default_name
-      @score = 0
-    else
-      super()
-    end
+  def initialize(game_mode, default_game: false)
+    @default_game = default_game
+    super(game_mode)
   end
 
   def choose
@@ -316,23 +324,22 @@ class Human < Player
     loop do
       prompt('choose_move')
       choice = gets.chomp
-      break if Move::VALUES.include? choice
+
+      break if valid_choice?(choice)
 
       prompt('invalid_move')
     end
 
-    self.move = Move.new(choice)
+    self.move = chosen_move(choice)
     prompt("#{name} chose #{move}.")
     pause
   end
 
   private
 
-  def set_default_name
-    self.name = 'Player'
-  end
-
   def set_name
+    return self.name = 'Player' if default_game
+
     answer = ''
 
     loop do
@@ -345,6 +352,20 @@ class Human < Player
 
     self.name = answer
   end
+
+  def chosen_move(choice)
+    case game_mode
+    when :rps   then ClassicMove.new(choice)
+    when :rpsls then ExpandedMove.new(choice)
+    end
+  end
+
+  def valid_choice?(choice)
+    case game_mode
+    when :rps   then ClassicMove::VALUES.include? choice
+    when :rpsls then ExpandedMove::VALUES.include? choice
+    end
+  end
 end
 
 class Computer < Player
@@ -355,16 +376,25 @@ class Computer < Player
   end
 
   def choose
-    self.move = Move.new(Move::VALUES.sample)
+    self.move = chosen_move
     prompt("#{name} chose #{move}.")
     pause
+  end
+
+  private
+
+  def chosen_move
+    case game_mode
+    when :rps then ClassicMove.new(ClassicMove::VALUES.sample)
+    when :rpsls then ExpandedMove.new(ExpandedMove::VALUES.sample)
+    end
   end
 end
 
 class Move
-  attr_reader :value
+  include Comparable
 
-  VALUES = ['rock', 'paper', 'scissors']
+  attr_reader :value
 
   def initialize(value)
     @value = value
@@ -374,31 +404,47 @@ class Move
     value
   end
 
-  def >(other_move)
-    (rock? && other_move.scissors?) ||
-      (paper? && other_move.rock?) ||
-      (scissors? && other_move.paper?)
-  end
-
-  def <(other_move)
-    (rock? && other_move.paper?) ||
-      (paper? && other_move.scissors?) ||
-      (scissors? && other_move.rock?)
+  def <=>(other_move)
+    if move.beats?(other_move)
+      1
+    elsif other_move.beats?(move)
+      -1
+    elsif move == other_move
+      0
+    end
   end
 
   protected
 
-  def scissors?
-    value == 'scissors'
+  def beats?(other_move)
+    self.class::WIN_RULES[value].include?(other_move.value)
   end
 
-  def rock?
-    value == 'rock'
+  def ==(other_move)
+    value == other_move.value
   end
 
-  def paper?
-    value == 'paper'
+  private
+
+  def move
+    self
   end
+end
+
+class ClassicMove < Move
+  VALUES = ['rock', 'paper', 'scissors']
+  WIN_RULES = { 'rock' => ['scissors'],
+                'paper' => ['rock'],
+                'scissors' => ['paper'] }
+end
+
+class ExpandedMove < Move
+  VALUES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+  WIN_RULES = { 'rock' => ['lizard', 'scissors'],
+                'paper' => ['rock', 'spock'],
+                'scissors' => ['paper', 'lizard'],
+                'lizard' => ['spock', 'paper'],
+                'spock' => ['scissors', 'rock'] }
 end
 
 # class Rule
