b3317db3de08e36e77b7247299fcc290d070ab1f
Aaron Tan <18561712+aaronwtan@users.noreply.github.com>
2024-08-06 14:37:36 -0500

    Add functionality to display board and welcome/goodbye messages; add modules for general utility and display methods

--- lesson_5/ttt.rb
+++ lesson_5/ttt.rb
@@ -1,9 +1,60 @@
+require 'pry'
+require 'yaml'
+
+module Displayable
+  private
+
+  def display_welcome_message
+    prompt('welcome')
+    puts ''
+  end
+
+  def display_goodbye_message
+    prompt('goodbye')
+  end
+
+  def display_board
+    puts ''
+    puts "     |     |"
+    puts "     |     |"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "     |     |"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "     |     |"
+    puts "     |     |"
+    puts ''
+  end
+end
+
+module Utility
+  include Displayable
+
+  MESSAGES = YAML.load_file('ttt.yml')
+
+  private
+
+  def prompt(msg)
+    if MESSAGES.key?(msg)
+      puts ">> #{MESSAGES[msg]}"
+    else
+      puts msg
+    end
+  end
+end
+
 class TTTGame
+  include Utility
+
   def play
     display_welcome_message
 
     loop do
       display_board
+      break
       first_player_moves
       break if someone_won? || board_full?
 
@@ -11,7 +62,7 @@ class TTTGame
       break if someone_won? || board_full?
     end
 
-    display_result
+    # display_result
     display_goodbye_message
   end
 end
@@ -28,7 +79,7 @@ class Square
   end
 end
 
-class Player
+class Player 
   def initialize
     
   end
@@ -38,3 +89,4 @@ class Player
   end
 end
 
+TTTGame.new.play
