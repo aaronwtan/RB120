174b38855c4e6850b1c501608d12096dfa0c0b8d
Aaron Tan <18561712+aaronwtan@users.noreply.github.com>
2024-08-22 16:50:06 -0500

    Add functionality to let player pick any marker and encapsulate marker state and behaviors into Player class

--- lesson_5/ttt/ttt.rb
+++ lesson_5/ttt/ttt.rb
@@ -1,9 +1,44 @@
 require 'pry'
 require 'yaml'
 
-module Displayable
-  private
+MESSAGES = YAML.load_file('ttt.yml')
 
+module Utility
+  def prompt(msg)
+    if MESSAGES.key?(msg)
+      puts ">> #{MESSAGES[msg]}"
+    else
+      puts ">> #{msg}"
+    end
+  end
+
+  def join_or(arr, delimiter=', ', word='or')
+    case arr.size
+    when 0 then ''
+    when 1 then arr.first.to_s
+    when 2 then arr.join(" #{word} ")
+    else
+      arr_copy = arr.dup
+      arr_copy[-1] = "#{word} #{arr_copy.last}"
+      arr_copy.join(delimiter)
+    end
+  end
+
+  def answered_yes?
+    loop do
+      answer = gets.chomp.strip.downcase
+      return %w(y yes).include?(answer) if %w(y yes n no).include?(answer)
+
+      prompt('invalid_yes_or_no')
+    end
+  end
+
+  def clear_screen
+    system 'clear'
+  end
+end
+
+module TTTGameDisplay
   def display_welcome_message
     prompt('welcome')
     puts ''
@@ -14,91 +49,102 @@ module Displayable
   end
 
   def display_board
-    system 'clear'
     prompt "You're a #{human.marker}. Computer is a #{computer.marker}."
     puts ''
-    puts "     |     |"
-    puts "  #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |  #{board.get_square_at(3)}"
-    puts "     |     |"
-    puts "-----+-----+-----"
-    puts "     |     |"
-    puts "  #{board.get_square_at(4)}  |  #{board.get_square_at(5)}  |  #{board.get_square_at(6)}"
-    puts "     |     |"
-    puts "-----+-----+-----"
-    puts "     |     |"
-    puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)}"
-    puts "     |     |"
+    board.draw
     puts ''
+  end
+
+  def clear_screen_and_display_board
+    clear_screen
+    display_board
   end
 
   def display_result
     display_board
 
-    case board.detect_winner
+    case board.winning_marker
     when human.marker    then prompt('human_won')
     when computer.marker then prompt('computer_won')
     else                      prompt 'tie'
     end
   end
-end
 
-module Utility
-  include Displayable
-
-  MESSAGES = YAML.load_file('ttt.yml')
-
-  private
-
-  def prompt(msg)
-    if MESSAGES.key?(msg)
-      puts ">> #{MESSAGES[msg]}"
-    else
-      puts msg
-    end
-  end
-
-  def join_or(arr, delimiter=', ', word='or')
-    case arr.size
-    when 0 then ''
-    when 1 then arr.first.to_s
-    when 2 then arr.join(" #{word} ")
-    else
-      arr[-1] = "#{word} #{arr.last}"
-      arr.join(delimiter)
-    end
+  def display_play_again_message
+    prompt('play_again')
+    puts ''
   end
 end
 
 class TTTGame
-  include Utility
+  include Utility, TTTGameDisplay
 
   attr_reader :board, :human, :computer
+  attr_accessor :current_marker
 
   HUMAN_MARKER = 'X'
   COMPUTER_MARKER = 'O'
 
+  FIRST_TO_MOVE = HUMAN_MARKER
+
   def initialize
     @board = Board.new
-    @human = Player.new(HUMAN_MARKER)
-    @computer = Player.new(COMPUTER_MARKER)
+    @human = Human.new
+    @computer = Computer.new
+    @current_marker = FIRST_TO_MOVE
   end
 
   def play
     display_welcome_message
-    display_board
-
-    loop do
-      human_moves
-      break if board.someone_won? || board.full?
-
-      computer_moves
-      break if board.someone_won? || board.full?
-
-      display_board
-    end
-
-    display_result
+    main_game
     display_goodbye_message
+  end
+
+  private
+
+  def main_game
+    loop do
+      display_board
+      player_move
+      display_result
+      break unless play_again?
+
+      reset
+      display_play_again_message
+    end
+  end
+
+  def play_again?
+    prompt('ask_play_again')
+    answered_yes?
+  end
+
+  def reset
+    board.reset
+    self.current_marker = FIRST_TO_MOVE
+  end
+
+  def player_move
+    loop do
+      current_player_moves
+      break if board.someone_won? || board.full?
+
+      clear_screen_and_display_board if human_turn?
+    end
+  end
+
+  def current_player_moves
+    if human_turn?
+      human_moves
+      self.current_marker = COMPUTER_MARKER
+    else
+      computer_moves
+      self.current_marker = HUMAN_MARKER
+    end
+  end
+
+  def human_turn?
+    current_marker == HUMAN_MARKER
   end
 
   def human_moves
@@ -112,15 +158,17 @@ class TTTGame
       prompt('invalid_choice')
     end
 
-    board.set_square_at(square, human.marker)
+    board[square] = human.marker
   end
 
   def computer_moves
-    board.set_square_at(board.unmarked_keys.sample, computer.marker)
+    board[board.unmarked_keys.sample] = computer.marker
   end
 end
 
 class Board
+  include Utility
+
   attr_reader :squares
 
   WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
@@ -128,16 +176,24 @@ class Board
                   [[1, 5, 9], [3, 5, 7]]              # diagonals
 
   def initialize
-    @squares = (1..9).each_with_object({}) do |key, hsh|
-      hsh[key] = Square.new
-    end
+    reset
   end
 
-  def get_square_at(key)
-    squares[key]
+  def draw # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+    puts "     |     |"
+    puts "  #{squares[1]}  |  #{squares[2]}  |  #{squares[3]}"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "  #{squares[4]}  |  #{squares[5]}  |  #{squares[6]}"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "  #{squares[7]}  |  #{squares[8]}  |  #{squares[9]}"
+    puts "     |     |"
   end
 
-  def set_square_at(key, marker)
+  def []=(key, marker)
     squares[key].marker = marker
   end
 
@@ -150,19 +206,28 @@ class Board
   end
 
   def someone_won?
-    !!detect_winner
+    !!winning_marker
   end
 
-  def detect_winner
+  def winning_marker
     WINNING_LINES.each do |line|
-      if line.all? { |key| squares[key].marker == TTTGame::HUMAN_MARKER }
-        return TTTGame::HUMAN_MARKER
-      elsif line.all? { |key| squares[key].marker == TTTGame::COMPUTER_MARKER }
-        return TTTGame::COMPUTER_MARKER
-      end
+      test_squares = squares.values_at(*line)
+      return test_squares.first.marker if three_identical_markers?(test_squares)
     end
 
     nil
+  end
+
+  def reset
+    clear_screen
+    @squares = (1..9).each_with_object({}) { |key, hsh| hsh[key] = Square.new }
+  end
+
+  private
+
+  def three_identical_markers?(test_squares)
+    markers = test_squares.select(&:marked?).map(&:marker)
+    markers.size == 3 && markers.uniq.size == 1
   end
 end
 
@@ -179,16 +244,76 @@ class Square
     marker
   end
 
+  def marked?
+    marker != INITIAL_MARKER
+  end
+
   def unmarked?
     marker == INITIAL_MARKER
   end
 end
 
 class Player
-  attr_reader :marker
+  include Utility
 
-  def initialize(marker)
-    @marker = marker
+  attr_reader :type
+  attr_accessor :name, :marker
+
+  @@available_markers = %w(X O)
+
+  def initialize
+    set_name
+    # set_type
+    set_marker
+  end
+
+  def self.available_markers
+    @@available_markers
+  end
+
+  private
+
+  def set_marker
+    self.marker = choose_marker
+  end
+
+  def choose_marker
+    markers = Player.available_markers
+    return markers.first if markers.size == 1
+
+    loop do
+      prompt("Choose #{join_or(markers)} for this player.")
+      choice = gets.chomp.upcase.strip
+      return markers.delete(choice) if markers.include?(choice)
+
+      prompt('invalid_choice')
+    end
+  end
+end
+
+class Human < Player
+  private
+
+  def set_name
+    prompt('ask_name')
+  end
+
+  def set_type
+    self.type = :human
+  end
+end
+
+class Computer < Player
+  NAMES = %w(HAL J.A.R.V.I.S Ultron Cortana Braniac Bender)
+
+  private
+
+  def set_name
+    self.name = NAMES.sample
+  end
+
+  def set_type
+    self.type = :computer
   end
 end
 
